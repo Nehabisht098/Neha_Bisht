c2c86cffdb4b0bcb83895c9cfa6a5dac83657682
Nehabisht098 <nehabisht2025@gmail.com>
2025-09-09 09:37:34 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int l=5;
+    int b=10;
+    int area=l*b;
+    int peri=2*(l+b);
+    printf("area is %d", area);
+    printf("perimeter is %d", peri);
+}
---
+++ sum from user.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int num1, num2, sum;
+    printf("enter two integers:");
+    scanf("%d %d", &num1,&num2);
+    sum = num1+num2;
+    printf("sum of %d and %d is: %d\n", num1, num2, sum);
+}
+
